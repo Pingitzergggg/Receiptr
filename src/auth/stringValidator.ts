aa7b83efc855a55fa0a27a9549a30f5833789a29
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-09-15 14:31:16 +0200

    stringValidator 2

--- src/auth/stringValidator.ts
+++ src/auth/stringValidator.ts
@@ -15,11 +15,16 @@ function countChars(input : string, char : string) : number {
     return countChar;
 }
 
+function stringNormalize(input : string) : string {
+    const getNFD : string = input.normalize('NFD');
+    return getNFD.replace(/[\u0300-\u036f]/g, '');
+}
+
 function checkInjection(input : string) : boolean {
     if (input.includes(' ') || input.includes(';')) {
         return true;
     } else {
-        return false;
+        throw 'Field cannot contain \' \' and \';\' characters!';
     }
 }
 
@@ -31,10 +36,47 @@ function mailValidator(input : string) : boolean {
             if (countChars(domain, '.') >= 1) {
                 return true;
             } else {
-                return false;
+                throw 'Email format not valid!';
             }
         } else {
-            return false;
+            throw 'Email format not valid!';
+        }
+    } else {
+        return false;
+    }
+}
+
+function phoneValidator(input : string) : boolean {
+    if (4 < input.length && input.length < 16) {
+        const digits : unknown[] = input.split('');
+        let convertedNumber : number = 0;
+        for (let digit in digits) {
+            convertedNumber= digits[digit] as number;
+            if (convertedNumber != convertedNumber + 0) {
+                throw 'Phone should only contain numbers!';
+            }
+        }
+        return true;
+    } else {
+        throw 'Digit length is not in supported range!';
+    }
+}
+
+//65-90 | 97-122
+function nameValidator(input : string) : boolean {
+    if (checkInjection(input)) {
+        if (input.length <= 45) {
+            const chars : unknown[] = input.split('');
+            let convertedNumber : number = 0;
+            for (let ch in chars) {
+                convertedNumber= chars[ch] as number;
+                if (convertedNumber == convertedNumber + 0) {
+                    throw 'Name should only contain letters!';
+                }
+            }
+        return true;
+        } else {
+            throw 'Field length exceeds limit!'
         }
     } else {
         return false;
