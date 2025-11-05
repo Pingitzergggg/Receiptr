f406cc124cb93ff918508feb8077ed7502249039
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-11-05 12:35:37 +0100

    Design

--- src/appfiles/ReceiptPanel.tsx
+++ src/appfiles/ReceiptPanel.tsx
@@ -54,8 +54,8 @@ function ReceiptPanel() : any {
   return (
     <>
       <Outlet />
-      <button className='upload-button btn btn-soft'>
-        +
+      <button className='upload-button btn btn-soft w-auto h-auto p-[1rem]'>
+        <svg className='size-[1rem]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#e6e6e6" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg>
       </button>
       <div className='container mt-20 mb-20' id='receipts-div'>
         <div id='receipt-grid' className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-15'>
