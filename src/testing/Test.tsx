aca99f7b84b9e8d668e890f8cd78668e960951a3
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-12-29 12:17:20 +0100

    receipt uploader redesign

--- src/testing/Test.tsx
+++ src/testing/Test.tsx
@@ -5,12 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import DeleteItem from "../tools/DeleteItem";
 import Select from "../tools/Select";
 import Button from "../tools/Button";
+import PdfViewer from "../tools/PdfViewer";
 
 function Test() : any {
 
-    return <div className="absolute-center">
-        <Button label="Upload" icon={<FontAwesomeIcon icon={faCheck} />} />
-        </div>
+    return (<>
+        <PdfViewer fileUrl="../../cats.pdf"  scrollable={false} />
+    </>);
 }
 
 export default Test;
